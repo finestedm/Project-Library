feb344e7b0bde3caeace9dfd1ceeea16266495d2
pawel <finestedm@gmail.com>
2022-05-20 19:40:37 +0200

    building up HTML part inside js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var myLibrary = [];
 
+
 function Book(title, author, pages, readPages, isRead) {
     this.title = title;
     this.author = author;
@@ -12,26 +13,20 @@ Book.prototype.addBookToLibrary = function () {
     myLibrary.push(this);
 };
 
-
-Book.prototype.deleteBookFromLibrary = function () {
-    myLibrary = myLibrary.filter((book) =>
-        compareBooks(book.title, book.author)
-    );
-};
-
-
-function compareBooks(currentBookTitle, currentBookAuthor) {
-    console.log(currentBookTitle);
+function compareBooks(toDeleteBookTitle, currentBookTitle, toDeleteBookAuthor, currentBookAuthor) {
     if (
-        this.title !== currentBookTitle ||
-        this.author !== currentBookAuthor
+        toDeleteBookTitle !== currentBookTitle ||
+        toDeleteBookAuthor !== currentBookAuthor
     ) {
         return currentBookTitle;
     }
 }
-compareBooks.prototype = Object.create(Book.prototype);
 
-
+Book.prototype.deleteBookFromLibrary = function () {
+    myLibrary = myLibrary.filter((book) =>
+        compareBooks(this.title, book.title, this.author, book.author)
+    );
+};
 
 const book1 = new Book("Rings", "Tolkien", 320, 12, false);
 const book2 = new Book("chleb", "pies", 333, 22, false);
@@ -45,3 +40,21 @@ book4.addBookToLibrary();
 
 book3.deleteBookFromLibrary();
 console.log(myLibrary);
+
+grid = document.querySelector('.grid');
+
+
+for (let i = 0; i < myLibrary.length; i++) { // this loop iterates through myLibrary
+    var card = document.createElement('card')  //creates card
+    Object.keys(myLibrary[i]).forEach(element => { //then iterates through all keys inside each element of myLibrary
+        var cardDescription = document.createElement(`div`); //and creates new element for each 
+        cardDescription.setAttribute('class', element); //sets class according to key description
+        card.appendChild(cardDescription); //and appends the card 
+    });
+    grid.appendChild(card)
+}
+
+
+
+
+
